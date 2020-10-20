267951bf66e36bf4d50bc20317f9938d9e87b820
EmanuelMiron <emanuel_miron6@gmail.com>
2020-10-20 23:17:55 +0200

    Updates

---
+++ precache-manifest.6d8ecd2d1e0eac2363fb70bba5b1dad6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5078418e44db5799331a89a87ba39aae",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "86a8c57e8c9427090423",
+    "url": "/Tetris/static/css/main.d80a99d2.chunk.css"
+  },
+  {
+    "revision": "5812d791e18b73080c2b",
+    "url": "/Tetris/static/js/2.011a972e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Tetris/static/js/2.011a972e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a8c57e8c9427090423",
+    "url": "/Tetris/static/js/main.0931e26e.chunk.js"
+  },
+  {
+    "revision": "99f69e32249178bbcf6a",
+    "url": "/Tetris/static/js/runtime-main.63b21ce3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris/static/media/bg.2810fe6b.png"
+  }
+]);
